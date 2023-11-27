54db582b4a22c915431433437a95868ed05377b1
developwithayush <developwithdubey@gmail.com>
2023-11-27 13:58:12 +0530

    Create components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import FormText from './Components/Form/FormText';
+import Alert from './Components/Alert/Alert';
+import { useState } from 'react';
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import About from './Components/About/About';
+
+
+
 
 function App() {
+  const [dark, setDark] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message) => {
+    setAlert({
+      msg: message,
+    }
+    )
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
+
+  const toggleMode = () => {
+    if (dark === "light") {
+      document.body.style.background = "black"
+      setDark("dark")
+      showAlert("darkmode enable")
+
+
+    }
+    else {
+      document.body.style.background = "white"
+
+      setDark("light")
+      showAlert("lightmode enable")
+
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    <>
+      <Navbar tittle="AyushText" mode={dark} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <FormText mode={dark} showAlert={showAlert} />
+      {/* <Routes> */}
+      {/* <Route path="/" element={<FormText mode={dark} showAlert={showAlert} />} /> */}
+      {/* <Route path="/about" element={<About />} /> */}
+      {/* </Routes> */}
+
+
+    </>
+  //  {/* </Router> */ }
+
   );
 }
 
